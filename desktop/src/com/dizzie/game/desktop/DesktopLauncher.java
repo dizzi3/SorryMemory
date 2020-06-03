2093f162830ed4ba7c1d741400454cb09a479d51
dizzi3 <michas2820@onet.eu>
2020-06-03 18:10:02 +0200

    Initial commit

---
+++ desktop/src/com/dizzie/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dizzie.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dizzie.game.MemoryGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MemoryGame(), config);
+	}
+}
