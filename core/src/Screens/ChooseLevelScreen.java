087b79ebfb51d37bbcbe6d34b2df76f7c246f462
dizzi3 <michas2820@onet.eu>
2020-06-06 18:52:35 +0200

    Finished Choose Level Screen

--- core/src/Screens/ChooseLevelScreen.java
+++ core/src/Screens/ChooseLevelScreen.java
@@ -1,8 +1,17 @@
 package Screens;
 
+import Styles.DefaultButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.dizzie.game.MemoryGame;
 
 public class ChooseLevelScreen extends AbstractScreen{
+
+    private Table table;
+
+    private TextButton lvlEasyButton;
+    private TextButton lvlMediumButton;
+    private TextButton lvlHardButton;
 
     public ChooseLevelScreen(MemoryGame game){
         super(game);
@@ -10,7 +19,33 @@ public class ChooseLevelScreen extends AbstractScreen{
 
     @Override
     protected void init() {
-
+        initializeButtons();
+        initializeTable();
     }
-    
+
+    private void initializeButtons() {
+        lvlEasyButton = new TextButton("Easy - 12 pairs", new DefaultButtonStyle());
+        lvlMediumButton = new TextButton("Medium - 18 pairs", new DefaultButtonStyle());
+        lvlHardButton = new TextButton("Hard - 24 pairs", new DefaultButtonStyle());
+    }
+
+    private void initializeTable(){
+        table = new Table();
+        table.setFillParent(true);
+        table.add(lvlEasyButton).padBottom(40);
+        table.row();
+        table.add(lvlMediumButton).padBottom(40);
+        table.row();
+        table.add(lvlHardButton);
+        stage.addActor(table);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        stage.act();
+        stage.draw();
+    }
+
 }
