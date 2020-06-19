7039666a37a924dd69183fd05600a4b40e7f4346
dizzi3 <michas2820@onet.eu>
2020-06-19 13:00:10 +0200

    Added TileClickListener functionality

--- core/src/Listeners/TileClickListener.java
+++ core/src/Listeners/TileClickListener.java
@@ -1,13 +1,80 @@
 package Listeners;
 
+import Tiles.Tile;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public class TileClickListener extends HandCursorListener{
+import java.util.concurrent.TimeUnit;
+
+public class TileClickListener extends ClickListener {
+
+    private static Tile lastClickedTile = null;
+    private Tile tile;
+
+    public TileClickListener(Tile tile){
+        this.tile = tile;
+    }
 
     @Override
     public void clicked(InputEvent event, float x, float y) {
         super.clicked(event, x, y);
 
-        System.out.println("CLICKED A TILE");
+        if(!tile.isClicked()) {
+            tile.setClicked(true);
+            tile.changeToTexture();
+            System.out.println("ZMIEN SIE");
+
+            if(lastClickedTile == null)
+                lastClickedTile = tile;
+            else{
+
+                lastClickedTile.setDisabled(true);
+                tile.setDisabled(true);
+
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                }catch(Exception e){}
+
+                lastClickedTile.setDisabled(false);
+                tile.setDisabled(false);
+
+                if(lastClickedTile.getTexture() == tile.getTexture()){
+
+                    //WE GOT A POINT
+                    System.out.println("WE GOT A POINT!!!");
+
+                }else{
+System.out.println("HEHE");
+                    //TODO: ADD NUMBER OF TRIES AND HIDE BOTH TEXTURES
+                    lastClickedTile.changeToCoveredTexture();
+                    tile.changeToCoveredTexture();
+
+                }
+
+                lastClickedTile = null;
+            }
+        }
+    }
+
+    @Override
+    public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+        super.enter(event, x, y, pointer, fromActor);
+
+        if(!tile.isClicked()) {
+            Pixmap pm = new Pixmap(Gdx.files.internal("other/hand_cursor.png"));
+            Gdx.graphics.setCursor(Gdx.graphics.newCursor(pm, 8, 0));
+            pm.dispose();
+        }
+    }
+
+    @Override
+    public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+        super.exit(event, x, y, pointer, toActor);
+
+        Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
     }
 }
--- core/src/Tiles/Tile.java
+++ core/src/Tiles/Tile.java
@@ -11,19 +11,38 @@ public class Tile extends ImageButton {
     private static final Texture COVERED_TEXTURE = new Texture(Gdx.files.internal("tiles/covered_tile.png"));
     private Texture texture;
 
+    private boolean clicked = false;
+
     public Tile(Texture texture) {
-        //super(new TextureRegionDrawable(COVERED_TEXTURE));
+        super(new TextureRegionDrawable(COVERED_TEXTURE));
 
         //TODO: DELETE THIS AND UNCOMMENT THE SUPPER ABOVE
-        super(new TextureRegionDrawable(texture));
-
+        //super(new TextureRegionDrawable(texture));
 
         this.texture = texture;
-        addListener(new TileClickListener());
+        addListener(new TileClickListener(this));
     }
 
     public Texture getTexture(){
         return this.texture;
     }
+
+    public void changeToTexture(){
+        changeTexture(texture);
+    }
+
+    public void changeToCoveredTexture(){
+        changeTexture(COVERED_TEXTURE);
+    }
+
+    private void changeTexture(Texture texture){
+        ImageButtonStyle style = new ImageButtonStyle();
+        style.up = new TextureRegionDrawable(texture);
+        setStyle(style);
+    }
+
+    public void setClicked(boolean clicked){ this.clicked = clicked; }
+
+    public boolean isClicked(){ return this.clicked; }
 
 }
