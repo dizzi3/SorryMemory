d07078bcd85f9820d51cc9af77123214cee2c034
dizzi3 <michas2820@onet.eu>
2020-06-19 20:34:54 +0200

    Fixed minor error

--- core/src/Tiles/RandomTileTexture.java
+++ core/src/Tiles/RandomTileTexture.java
@@ -2,8 +2,6 @@ package Tiles;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.sun.tools.javac.util.List;
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -14,22 +12,22 @@ public class RandomTileTexture
  * żeby nie wystąpiła sytuacja w której dana tekstura zostanie pobrana dwa razy*/{
 
     public static ArrayList<Integer> alreadyUsedTextureIndexes = new ArrayList<>();
-    private static final ArrayList<Texture> textures = new ArrayList<>(List.of(
-            new Texture(Gdx.files.internal("tiles/pizza.png")),
-            new Texture(Gdx.files.internal("tiles/zombie.png")),
-            new Texture(Gdx.files.internal("tiles/telephone.png")),
-            new Texture(Gdx.files.internal("tiles/alien.png")),
-            new Texture(Gdx.files.internal("tiles/baloon.png")),
-            new Texture(Gdx.files.internal("tiles/cartoon_girl.png")),
-            new Texture(Gdx.files.internal("tiles/haunted_house.png")),
-            new Texture(Gdx.files.internal("tiles/lion.png")),
-            new Texture(Gdx.files.internal("tiles/printer.png")),
-            new Texture(Gdx.files.internal("tiles/pumpkin.png")),
-            new Texture(Gdx.files.internal("tiles/turtle.png")),
-            new Texture(Gdx.files.internal("tiles/virus.png"))
-    ));
 
     public static Texture get(){
+
+        ArrayList<Texture> textures = new ArrayList<>();
+        textures.add(new Texture(Gdx.files.internal("tiles/pizza.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/zombie.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/telephone.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/alien.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/baloon.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/cartoon_girl.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/haunted_house.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/lion.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/printer.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/pumpkin.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/turtle.png")));
+        textures.add(new Texture(Gdx.files.internal("tiles/virus.png")));
 
         Random rand = new Random();
         Texture texture;
